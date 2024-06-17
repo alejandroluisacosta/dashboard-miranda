38dcecade4f139158884059715d424435b812890
Alejandro <alejandroluisacosta95@gmail.com>
2024-06-17 18:05:04 +0200

    add: login auth

--- MirandaDashboard/src/App.jsx
+++ MirandaDashboard/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
@@ -13,22 +13,33 @@ import AddUserPage from './Pages/AddUserPage/AddUserPage'
 import LoginPage from './Pages/LoginPage/LoginPage'
 import ProtectedRoute from './Components/ProtectedRouteComponent/ProtectedRoute'
 
-const auth = false;
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="login" element={<LoginPage />} />
-        <Route path="" element={<ProtectedRoute auth={auth}><DashboardPage /></ProtectedRoute>} />
-        <Route path="bookings" element={<ProtectedRoute auth={auth}><BookingsPage /></ProtectedRoute>} />
-        <Route path="bookings/:id" element={<ProtectedRoute auth={auth}><BookingDetailsPage /></ProtectedRoute>} />
-        <Route path="rooms" element={<ProtectedRoute auth={auth}><RoomsPage /></ProtectedRoute>} />
-        <Route path="rooms/add" element={<ProtectedRoute auth={auth}><AddRoomPage /></ProtectedRoute>} />
-        <Route path="users" element={<ProtectedRoute auth={auth}><UsersPage /></ProtectedRoute>} />
-        <Route path="users/add" element={<ProtectedRoute auth={auth}><AddUserPage /></ProtectedRoute>} />
-        <Route path="contact" element={<ProtectedRoute auth={auth}><ContactPage /></ProtectedRoute>} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>,
-)
+const App = () => {
+
+  const [auth, setAuth] = useState(false);
+  const authHandler = () => {
+    setAuth(true);
+  }
+    console.log(auth);
+    return (
+      <>
+        <React.StrictMode>
+          <BrowserRouter>
+            <Routes>
+              <Route path="login" element={<LoginPage authHandler={authHandler}/>} />
+              <Route path="" element={<ProtectedRoute auth={auth}><DashboardPage /></ProtectedRoute>} />
+              <Route path="bookings" element={<ProtectedRoute auth={auth}><BookingsPage /></ProtectedRoute>} />
+              <Route path="bookings/:id" element={<ProtectedRoute auth={auth}><BookingDetailsPage /></ProtectedRoute>} />
+              <Route path="rooms" element={<ProtectedRoute auth={auth}><RoomsPage /></ProtectedRoute>} />
+              <Route path="rooms/add" element={<ProtectedRoute auth={auth}><AddRoomPage /></ProtectedRoute>} />
+              <Route path="users" element={<ProtectedRoute auth={auth}><UsersPage /></ProtectedRoute>} />
+              <Route path="users/add" element={<ProtectedRoute auth={auth}><AddUserPage /></ProtectedRoute>} />
+              <Route path="contact" element={<ProtectedRoute auth={auth}><ContactPage /></ProtectedRoute>} />
+            </Routes>
+          </BrowserRouter>
+        </React.StrictMode>,
+      </>
+    )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
--- MirandaDashboard/src/Pages/LoginPage/LoginPage.jsx
+++ MirandaDashboard/src/Pages/LoginPage/LoginPage.jsx
@@ -1,7 +1,29 @@
-const LoginPage = () => {
+import { useNavigate } from "react-router-dom"
+
+
+const LoginPage = ({ authHandler }) => {
+
+    const navigate = useNavigate();
+    
+    const submitHandler = (event) => {
+        event.preventDefault()
+        console.log(event.target.elements.username.value)
+        if (event.target.elements.username.value === 'John') {
+            authHandler();
+            navigate('/');
+        }
+    }
+
     return (
         <>
-            <h1>Login page</h1>
+            <h1>Login</h1>
+            <form onSubmit={submitHandler}>
+                <label htmlFor="username">Username</label>
+                <input type="text" id="username" placeholder="Your username"/>
+                <label htmlFor="password">Password</label>
+                <input type="password" id="password" placeholder="Your password" autoComplete="true"/>
+                <button type="submit">Login</button>
+            </form>
         </>
     )
 }
