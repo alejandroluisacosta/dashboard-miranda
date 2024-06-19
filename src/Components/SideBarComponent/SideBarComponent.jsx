7c7c08e0562e591ab9591068ad5084c5d8f03881
Alejandro <alejandroluisacosta95@gmail.com>
2024-06-19 12:36:53 +0200

    add: style improvements on sidebar

--- src/Components/SideBarComponent/SideBarComponent.jsx
+++ src/Components/SideBarComponent/SideBarComponent.jsx
@@ -13,7 +13,10 @@ const SideBar = styled.div`
     max-width: 345px;
     background-color: white;
 
-    .logo-container {
+`;
+
+const LogoContainer = styled.div`
+    
         display: flex;
         align-items: center;
         margin-bottom: 60px;
@@ -22,74 +25,95 @@ const SideBar = styled.div`
             width: 48px;
             height: 48px;
         }
-    }
-
-    .text-container {
-
-        p:first-child {
-            font-size: 1.5rem;
-            font-weight: bold;
-            margin: 0;
-        }
-
-        p:last-child {
-            font-size: 12px;
-            margin: 0;
-        }
-    }
-
-    .navigation {
-
-        display: flex;
-        flex-direction: column;
-        margin-bottom: 40px;
-        color: var(--ocher-green);
-        .link-container {
-            display: flex;
-            align-items: center;
-            cursor: pointer;
-            margin-bottom: 55px;
-            .icon {
-                width: 28px;
-                height: 28px;
-                margin-right: 26px;
-            }
-            p {
+        div {
+            p:first-child {
+                font-size: 24px;
+                font-weight: bold;
                 margin: 0;
-                font-size: 18px;
+            }
+    
+            p:last-child {
+                font-size: 12px;
+                margin: 0;
             }
         }
-    }
+`;
 
-    .user-container {
-        text-align: center;
-        padding: 0 35px 24px;
-        box-shadow: 0px 20px 30px #00000014;
-        border-radius: 18px;
-        img {
-            width: 70px;
-            height: 70px;
-            border-radius: 8px;
-            transform: translateY(-50%);
-        }
-        .name {
-            letter-spacing: 0px;
-            color: #393939;
-            opacity: 1;
-            margin-top: -15px;
-            font-size: 16px;
-            margin-bottom: 9px;
-        }
-        .email {
-            margin-bottom: 16px;
-        }
-        button {
-            padding: 16px 42px;
-            border-radius: 8px;
-            border: 0;
-            background-color: var(--lighter-green);
-            color: var(--dark-green);
-        }
+const Navigation = styled.div`
+
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 40px;
+    color: var(--ocher-green);
+
+`;
+
+const NavigationLink = styled.div`
+
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+    margin-bottom: 55px;
+    color: ${props => props.selected ? 'var(--red)' : 'inherit'};
+    .icon {
+        width: 28px;
+        height: 28px;
+        margin-right: 26px;
+    }
+    p {
+        margin: 0;
+        font-size: 18px;
+    }
+    
+`;
+
+const User = styled.div`
+
+    text-align: center;
+    padding: 0 35px 24px;
+    box-shadow: 0px 20px 30px #00000014;
+    border-radius: 18px;
+    margin-bottom: 60px;
+    img {
+        width: 70px;
+        height: 70px;
+        border-radius: 8px;
+        transform: translateY(-50%);
+    }
+    p:nth-child(2) {
+        letter-spacing: 0px;
+        color: #393939;
+        opacity: 1;
+        margin-top: -15px;
+        font-size: 16px;
+        margin-bottom: 9px;
+    }
+    p:nth-child(3) {
+        margin-bottom: 16px;
+    }
+    button {
+        padding: 16px 42px;
+        border-radius: 8px;
+        border: 0;
+        background-color: var(--lighter-green);
+        color: var(--dark-green);
+    }
+`;
+
+const Footer = styled.div`
+    p:first-child {
+        font-size: 16px;
+        font-weight: 600;
+        margin-bottom: 5px;
+        color: black;
+    }
+    p {
+        font-size: 14px;
+        color: var(--ocher-green);
+        font-weight: 300;
+    }
+    p:nth-child(2) {
+        margin-bottom: 65px;
     }
 `;
 
@@ -102,45 +126,44 @@ const SideBarComponent = () => {
     }
 
     return (
-        <>
-            <SideBar>
-                <div className="logo-container">
-                    <FaHotel className="icon"/>
-                    <div className="text-container">
-                        <p>travl</p>
-                        <p>Hotel Admin Dashboard</p>
-                    </div>
+        <SideBar>
+            <LogoContainer>
+                <img src="/assets/HotelLogo.svg" className="icon"/>
+                <div>
+                    <p>travl</p>
+                    <p>Hotel Admin Dashboard</p>
                 </div>
-                <div className="navigation">
-                    <div className="link-container" onClick={navigateHandler}>
-                        <MdOutlineDashboard className="icon"/>
-                        <p>Dashboard</p>
-                    </div>
-                    <div className="link-container" onClick={navigateHandler}>
-                        <TfiKey className="icon"/>
-                        <p>Room</p>
-                    </div>
-                    <div className="link-container" onClick={navigateHandler}>
-                        <LuCalendarCheck2 className="icon"/>
-                        <p>Bookings</p>
-                    </div>
-                    <div className="link-container" onClick={navigateHandler}>
-                        <CiUser className="icon"/>
-                        <p>Guest</p>
-                    </div>
-                    <div className="link-container" onClick={navigateHandler}>
-                        <HiOutlinePuzzle className="icon"/>
-                        <p>Users</p>
-                    </div>
-                </div>
-                <div className="user-container">
-                    <img src="/assets/user.jpeg"/>
-                    <p className="name">William Johanson</p>
-                    <p className="email">williajohn@mail.com</p>
-                    <button>Contact Us</button>
-                </div>
-            </SideBar>
-        </>
+            </LogoContainer>
+            <Navigation>
+                <NavigationLink selected={true} onClick={navigateHandler}>
+                    <MdOutlineDashboard className="icon"/>
+                    <p>Dashboard</p>
+                </NavigationLink>
+                <NavigationLink selected={false} onClick={navigateHandler}>
+                    <TfiKey className="icon"/>
+                    <p>Room</p>
+                </NavigationLink>
+                <NavigationLink selected={false} onClick={navigateHandler}>
+                    <LuCalendarCheck2 className="icon"/>
+                    <p>Bookings</p>
+                </NavigationLink>
+                <NavigationLink selected={false} onClick={navigateHandler}>
+                    <HiOutlinePuzzle className="icon"/>
+                    <p>Users</p>
+                </NavigationLink>
+            </Navigation>
+            <User>
+                <img src="/assets/user.jpeg"/>
+                <p>William Johanson</p>
+                <p>williajohn@mail.com</p>
+                <button>Contact Us</button>
+            </User>
+            <Footer>
+                <p>Travl Hotel Admin Dashboard</p>
+                <p>© 2020 All Rights Reserved</p>
+                <p>Made by Alejandro Luis Acosta</p>
+            </Footer>
+        </SideBar>
     )
 }
 
