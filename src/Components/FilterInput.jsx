ae3e8ab39f2261e71dc8e89c5281259411edfd1c
Alejandro <alejandroluisacosta95@gmail.com>
2024-06-20 13:24:21 +0200

    add: minor changes

--- src/Components/FilterInput.jsx
+++ src/Components/FilterInput.jsx
@@ -3,11 +3,12 @@ import styled from "styled-components";
 const StyledInput = styled.input`
     height: 100%;
     margin-top: 12px;
+    padding-left: 5px;
 `;
 
 const FilterInput = ({ filterByName }) => {
     return (
-        <StyledInput onChange={filterByName} type="text"/>
+        <StyledInput placeholder="Filter by name" onChange={filterByName} type="text"/>
     )
 }
 
--- src/Pages/RoomsPage/RoomsPage.jsx
+++ src/Pages/RoomsPage/RoomsPage.jsx
@@ -1,7 +1,89 @@
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import SideBarComponent from "../../Components/SideBarComponent/SideBarComponent";
+import FilterTabs from "../../Components/FilterTabs";
+import TableComponent from "../../Components/TableComponent/TableComponent";
+import Header from "../../Components/Header";
+
+const StyledNameColumn = styled.div`
+    img {
+        width: 150px;
+        height: 77px;
+    }
+`;
+
+const columns = [
+    {
+      label: 'Room Name',
+      display: row => (
+        <StyledNameColumn>
+          <img src="/assets/HotelRoom3.jpeg"/>
+          <div>
+            <p>{row.name}</p>
+            <p>{row.id}</p>
+          </div>
+        </StyledNameColumn>
+      )
+    },
+    {
+      label: 'Room Type',
+      property: 'room type'
+    },
+    {
+      label: 'Amenities',
+      property: 'amenities'
+    },
+    {
+      label: 'Price',
+      property: 'price'
+    },
+    {
+      label: 'Offer',
+      property: 'offer'
+    },
+    {
+      label: 'Status',
+      display: status => (
+        status === 'Available' ? <button>Available</button> : <button>Booked</button>
+      )
+    },
+  ];
+
+const StyledRooms = styled.div`
+
+`;
+
 const RoomsPage = () => {
+
+    let isLoggedIn = localStorage.getItem('token');
+
+    const navigate = useNavigate();
+    const logoutHandler = () => {
+        localStorage.setItem('token', 'false');
+        navigate('login');
+    }
+
     return (
         <>
-            <h1>RoomsPage</h1>
+            {
+            isLoggedIn && isLoggedIn !== 'false' ? 
+            <>
+                <StyledRooms>
+                    <div className="page-container">
+                        <SideBarComponent/>
+                        <div className="main-content">
+                          <Header/>
+                          <div className="filter-container">
+                            <FilterTabs/>
+                          </div>
+                          {/* <TableComponent/> */}
+                        </div>
+                    </div>
+                </StyledRooms>
+            </>
+            :
+            <Navigate to="/login"/>
+            }
         </>
     )
 }
