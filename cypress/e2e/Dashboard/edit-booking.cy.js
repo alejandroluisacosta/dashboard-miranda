6b4cbe2750af4c47ebb10e0cb8cc3d9d9c7db890
Alejandro <alejandroluisacosta95@gmail.com>
2024-07-04 18:01:34 +0200

    add: edit mode check

---
+++ cypress/e2e/Dashboard/edit-booking.cy.js
@@ -0,0 +1,18 @@
+/// <reference types="cypress" />
+
+describe('Check edit-booking functionality', () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:5173/')
+      })
+
+      it('Check that p element is changed for input element when on edit mode', () => {
+        cy.get('#username').type('John')
+        cy.get('#password').type(1234);
+        cy.get('form').submit();
+        cy.get('a[href="/bookings"]').click()
+        cy.get('a[href="/bookings/1234"]').click()
+        cy.get('.edit').click()
+        cy.get('div').contains('p', 'Check In').parent('div').find('input').should('exist');
+        cy.get('div').contains('p', 'Check In').parent('div').should('not.contain', 'p');
+      })
+})
