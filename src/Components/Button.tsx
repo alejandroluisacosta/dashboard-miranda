b35a2ab76581036b1e016e2d5d89083e24ad4dba
Alejandro <alejandroluisacosta95@gmail.com>
2024-07-09 11:39:10 +0200

    add: button component and button.test to typescript

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -1,12 +1,21 @@
 import styled from "styled-components";
 
-const StyledButton = styled.button`
+interface StyledButtonProps {
+    $isEditing: boolean;
+}
+
+const StyledButton = styled.button<StyledButtonProps>`
     background-color: ${props => props.$isEditing ? 'rgb(0, 0, 0)' : '#EBF1EF'} !important;
     color: ${props => props.$isEditing ? '#EBF1EF' : 'black' } !important;
     right: 30% !important;
 `;
 
-const Button = ({ content, $isEditing }) => {
+interface ButtonProps {
+    content: string;
+    $isEditing: boolean;
+}
+
+const Button: React.FC<ButtonProps> = ({ content, $isEditing }) => {
     return (
         <StyledButton $isEditing={$isEditing}>{content}</StyledButton>
     )
